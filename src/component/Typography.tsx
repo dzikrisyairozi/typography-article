65f653c4f39850fa8d1a98893c82eea6a6faf4f9
dikiogres <dikiogres@gmail.com>
2023-01-05 12:17:43 +0700

    feat: typography component with any types

--- src/component/Typography.tsx
+++ src/component/Typography.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import cn from "classnames";
+// import "./typography.css";
 
-const Typography = () => {
+const variantsMapping:any = {
+  h1: "h1",
+  h2: "h2",
+  h3: "h3",
+  h4: "h4",
+  h5: "h5",
+  h6: "h6",
+  subheading1: "h6",
+  subheading2: "h6",
+  body1: "p",
+  body2: "p",
+};
+
+const Typography = ({ variant, color, children, ...props }:any) => {
+  const Component = variant ? variantsMapping[variant] : "p";
+
   return (
-    <div>Typography</div>
-  )
-}
+    <Component
+      className={cn({
+        [`typography--variant-${variant}`]: variant,
+        [`typography--color-${color}`]: color,
+      })}
+      {...props}
+    >
+      {children}
+    </Component>
+  );
+};
 
-export default Typography
+export default Typography;
